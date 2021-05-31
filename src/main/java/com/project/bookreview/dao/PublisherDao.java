e0fca2b14a53dacada3c49c331290c904e4b078d
Ashu@test <69616825+Ashu-Test@users.noreply.github.com>
2021-05-31 20:28:07 +0530

    added addReview method

--- src/main/java/com/project/bookreview/dao/PublisherDao.java
+++ src/main/java/com/project/bookreview/dao/PublisherDao.java
@@ -6,8 +6,11 @@
 package com.project.bookreview.dao;
 
 import com.project.bookreview.entities.Publisher;
+import com.project.bookreview.entities.Review;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 /**
@@ -50,6 +53,68 @@ public class PublisherDao {
         return pub!=null;
     }
     
+     // get publisher by unique email id
+     public Publisher getPublisherByEmail(String email){
+         Publisher pub=null;
+         
+         
+           String query="from Publisher where publisherEmail =:e"; 
+            Session session=this.factory.openSession();
+            
+            Query q=session.createQuery(query);
+            q.setParameter("e", email);
+            pub=(Publisher) q.uniqueResult();
+            session.close();
+         
+         
+         
+         
+         
+         return pub;
+        
+     }
+     
+    
+     
+     
+     
+     
+     
+     // method to add review  by a publisher
+     public int addReview(Review rev){
+
+
+
+        int rId=Integer.MIN_VALUE;
+        
+        try{
+           Session ss=this.factory.openSession();
+      Transaction tx=ss.beginTransaction();
+          rId  =    (int) ss.save(rev);
+       tx.commit();
+       ss.close();
+  
+           } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return rId;
+
+
+
+
+}
+
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
 }
 
 
